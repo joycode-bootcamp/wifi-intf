7ecf8e196e99490dff1aa822db77c9931c467951
MagicBowen <e.bowen.wang@icloud.com>
2020-09-27 20:54:42 +0800

    first commit

---
+++ wifi/wifi.h
@@ -0,0 +1,17 @@
+#ifndef H95E613B3_B293_451C_8DA4_EE2CFFD3CC1C
+#define H95E613B3_B293_451C_8DA4_EE2CFFD3CC1C
+
+#include "mcl/stdc.h"
+
+MCL_STDC_BEGIN
+
+typedef struct WifiMsg{
+	int x, y, action;
+}WifiMsg;
+
+void send_msg(const WifiMsg* msg);
+void send_json(const char* json);
+
+MCL_STDC_END
+
+#endif
